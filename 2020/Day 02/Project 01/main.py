6a03b009281181022fc55181fae47793e0aa4230
Viktor Kruug <schreiaa@gmail.com>
2020-12-02 17:03:10 -0600

    Started on Day 2

---
+++ 2020/Day 02/Project 01/main.py
@@ -0,0 +1,26 @@
+# First number is minimum count
+# Second number is maximum count
+# Third element is the letter
+import re
+
+def isPasswordValid(min, max, let, password):
+  
+  return something
+
+minCount = 0
+maxCount = 0
+letter = 'a'
+password = 'password'
+validCount = 0
+
+inputPasswords = []
+
+with open('input.txt') as inputFile:
+  inputLines = inputFile.readlines()
+
+for inputs in inputLines:
+  inputPasswords.append(re.split('- :\n',inputs))
+
+for passwords in inputPasswords:
+  if isPasswordValid(passwords[0], passwords[1], passwords[2], passwords3):
+    validCount++
