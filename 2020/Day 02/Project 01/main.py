ec2a98d932202607eb880ec741610293278d76b8
Viktor Kruug <schreiaa@gmail.com>
2020-12-03 10:57:28 -0600

    Day 2 Project 1 is done.

--- 2020/Day 02/Project 01/main.py
+++ 2020/Day 02/Project 01/main.py
@@ -1,11 +1,21 @@
 # First number is minimum count
 # Second number is maximum count
 # Third element is the letter
-import re
+def splitLine(line):
+  inputsSplit = line.split()
+  numbersSplit = inputsSplit[0].split('-')
+  output = [numbersSplit[0], numbersSplit[1], inputsSplit[1].split(':')[0], inputsSplit[2]]
+  return output
 
-def isPasswordValid(min, max, let, password):
-  
-  return something
+def isPasswordValid(minimum, maximum, let, password):
+  letterCount = password.count(let)
+  #print(letterCount, minimum, maximum)
+  if ((letterCount >= int(minimum)) and (letterCount <= int(maximum))):
+    result = True
+  else:
+    result = False
+  #print(result)
+  return result
 
 minCount = 0
 maxCount = 0
@@ -14,13 +24,24 @@ password = 'password'
 validCount = 0
 
 inputPasswords = []
+validPasswords = []
 
 with open('input.txt') as inputFile:
   inputLines = inputFile.readlines()
 
 for inputs in inputLines:
-  inputPasswords.append(re.split('- :\n',inputs))
+  inputsSplit = splitLine(inputs)
 
-for passwords in inputPasswords:
-  if isPasswordValid(passwords[0], passwords[1], passwords[2], passwords3):
-    validCount++
+  minCount = inputsSplit[0]
+  maxCount = inputsSplit[1]
+  letter = inputsSplit[2]
+  password = inputsSplit[3]
+  #print(minCount, maxCount, letter, password)
+  result = isPasswordValid(minCount, maxCount, letter, password)
+
+  if result:
+    validCount = validCount + 1
+    validPasswords.append(password)
+
+print(validCount)
+#print(validPasswords)
